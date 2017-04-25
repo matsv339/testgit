d88708572395b6acdf5080185804e169abad0c89
matsv339 <matsv339@student.liu.se>
2017-04-25 23:32:38 +0200

    fix2

--- web/app.py
+++ web/app.py
@@ -1,9 +1,12 @@
 import os
 
+
 DEBUG = os.environ.get("DEBUG", "false") == "true"
+
 
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -17,7 +20,7 @@ def one():
 
 @app.route("two")
 def two():
-    return "two = 2"
+    return "two"
 
 
 @app.route("three")
